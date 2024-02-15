754139bd732def5a8afe1e64db4e0262e9218d23
Sai Praneeth Maddi <saipraneeth.maddi@gmail.com>
2024-02-15 21:32:30 +0530

    Initial commit

---
+++ project/src/test/java/com/cloudbees/project/TrainTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudbees.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
